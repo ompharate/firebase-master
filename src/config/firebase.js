af88064fc8c16c6e04806d2ad2066d056c2d455a
ompharate31 <ompharate31@gmail.com>
2024-04-07 22:56:48 +0530

    initalized repo

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQQT7nC5BxJzyNBR5d6_HUjEltRH0qabM",
+  authDomain: "dsa-project-3dca5.firebaseapp.com",
+  projectId: "dsa-project-3dca5",
+  storageBucket: "dsa-project-3dca5.appspot.com",
+  messagingSenderId: "11969764639",
+  appId: "1:11969764639:web:4fda378916c2245205a42a"
+};
+
+
+const app = initializeApp(firebaseConfig);
